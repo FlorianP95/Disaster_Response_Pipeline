46f62601431d14248b34b06957692e421336046f
puet_fl <florian.puetz@dlr.de>
2022-03-29 11:05:36 +0200

    added if_exists to the to_sql function -> overwrite the SQLite table if it already exists

--- data/process_data.py
+++ data/process_data.py
@@ -68,7 +68,7 @@ def save_data(df, database_filename):
         database_filename (str): Filename of the SQL database.
     """
     engine = create_engine("sqlite:///"+database_filename)
-    df.to_sql("DisasterResponse.db", engine, index=False)
+    df.to_sql("DisasterResponse.db", engine, index=False, if_exists="replace")
 
 
 def main():
